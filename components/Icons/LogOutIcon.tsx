73950b78b303feda1eee6ea944aefb6188e4a223
Antonio Cestari <antoniojcp96@gmail.com>
2023-01-05 17:35:40 -0300

    Adds Edit, Hamburger and LogOut icons

---
+++ components/Icons/LogOutIcon.tsx
@@ -0,0 +1,19 @@
+import { SVGProps } from "react";
+
+const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    stroke="#000"
+    strokeWidth={2}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4M16 17l5-5-5-5M21 12H9" />
+  </svg>
+);
+
+export default SvgComponent;
